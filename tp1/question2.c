2905fdebaf8b4e709b30d79f3724b14d9129292b
abderrazekbhr <abderrazek455@gmail.co>
2024-10-01 10:09:33 +0200

    question 2

--- tp1/question2.c
+++ tp1/question2.c
@@ -1,26 +1,38 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/mman.h>
-#include<fcntl.h>
-#include<unistd.h>
-#include<sys/stat.h>
-int main(){
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+int main()
+{
     struct stat fileStat;
-    int file=open("test.txt",O_RDONLY);
-    if(file==-1){
+    int file = open("test.txt", O_RDONLY | O_RDWR);
+    if (file == -1)
+    {
         perror("Error");
         exit(1);
     }
-    int excStat=stat("test.txt",&fileStat);
-    if(excStat==-1){
+    int excStat = stat("test.txt", &fileStat);
+    if (excStat == -1)
+    {
         perror("stat");
         exit(1);
     }
-    char *mmaped = (char *)mmap(NULL, excStat, PROT_READ | PROT_WRITE, MAP_SHARED, file, 0);
-    if(mmaped==MAP_FAILED){
+    printf("size of file: %ld\n", fileStat.st_size);
+    char *mmaped = (char *)mmap(NULL, fileStat.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, file, 0);
+    if (mmaped == MAP_FAILED)
+    {
         perror("mmap");
         exit(1);
     }
-    printf("addresse de mmaped: %p\n", (void *)&mmaped);
+    for (int i = 0; i < fileStat.st_size / 2; i++)
+    {
+        char temp = mmaped[i];
+        mmaped[i] = mmaped[fileStat.st_size - i - 1];
+        mmaped[fileStat.st_size - i - 1] = temp;
+    }
+    munmap(mmaped, fileStat.st_size);
+    close(file);
     return 0;
 }
