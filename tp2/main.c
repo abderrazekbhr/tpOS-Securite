250b17313cdbdac0abb78b1681db05b61fb663c3
Nyries <cyprien.gaborieau@gmail.com>
2024-10-09 10:17:34 +0200

    free 2.

--- tp2/main.c
+++ tp2/main.c
@@ -12,7 +12,9 @@ typedef struct HEADER_TAG {
 
 typedef struct FREE_MEMORY_BLOCK{
     struct FREE_MEMORY_BLOCK *ptr_next;
-} FREE_MEMORY_BLOCK;
+    HEADER *address;
+} Free_memory_block;
+Free_memory_block *free_head = NULL;
 
 void* malloc_3is(size_t memorySize){
     size_t total_size = sizeof(HEADER) + memorySize +  sizeof(MAGIC_NUMBER);
@@ -28,17 +30,60 @@ void* malloc_3is(size_t memorySize){
     }
     return new_block;
 }
-void free_3is(void* ptr){
 
+void free_3is(void* ptr){
+    Free_memory_block *block;
+    
+    block->ptr_next = NULL;
+    block->address = ptr;
+    if (free_head == NULL){
+        free_head = block;
+        return;
+    } 
+    Free_memory_block *temp = free_head;
+    while (temp->ptr_next){
+        temp = temp->ptr_next;
+    }
+    temp->ptr_next = block;
 }
+
+void displayFreeMemoryBlock()
+{
+    Free_memory_block *temp = free_head;
+    while (temp != NULL)
+    {
+        sleep(1);
+        printf("address : %p\n", (void *)temp->address);
+        temp = temp->ptr_next;
+    }
+}
+
 int main(){
     // Validating first step
     size_t size = 30;
     printf("magic_number : %ld\n", sizeof(MAGIC_NUMBER));
     printf("header : %ld\n", sizeof(HEADER));
-    void* memoryAlloc = malloc_3is(size);
+    void* memory_alloc0 = malloc_3is(size);
     void* end = sbrk(0);
-    printf("init : %p\n", memoryAlloc);
+    printf("init : %p\n", memory_alloc0);
     printf("end : %p\n", end);
+    void *memory_alloc1 = malloc_3is(size-5);
+    end = sbrk(0);
+    printf("init : %p\n", memory_alloc1);
+    printf("end : %p\n", end);
+    void *memory_alloc2 = malloc_3is(size-10);
+    end = sbrk(0);
+    printf("init : %p\n", memory_alloc2);
+    printf("end : %p\n", end);
+    void *memory_alloc3 = malloc_3is(size+5);
+    end = sbrk(0);
+    printf("init : %p\n", memory_alloc3);
+    printf("end : %p\n", end);
+
+    // Validating second step
+    free_3is(memory_alloc0);
+    free_3is(memory_alloc2);
+    displayFreeMemoryBlock();
+
     return 0;
 }
