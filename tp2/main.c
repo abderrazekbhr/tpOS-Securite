49921a765d12d5d939cab519e0bff442aa61f5c7
abderrazekbhr <abderrazek455@gmail.co>
2024-10-09 18:06:40 +0200

    add reuse function

--- tp2/main.c
+++ tp2/main.c
@@ -53,14 +53,38 @@ void free_3is(HEADER *ptr)
 
 void displayFreeMemoryBlock()
 {
+    printf("------------------------ Start display free memory block ------------------------\n");
     HEADER *temp = free_head;
-    while (temp->ptr_next != NULL)
+    while (temp != NULL)
     {
-        sleep(1);
-        printf("address : %p\n", temp);
-        printf("address : %p\n", (void *)temp->ptr_next);
+        printf("address : %p\n", (void *)temp);
         temp = temp->ptr_next;
     }
+    printf("------------------------ End display free memory block ------------------------\n");
+}
+void *reuseMemoryBlock(size_t memory_size)
+{
+    HEADER *temp = free_head;
+    if (temp == NULL)
+    {
+        return NULL;
+    }
+    else if (temp->bloc_size == memory_size)
+    {
+        free_head = temp->ptr_next;
+        return temp;
+    }
+    while (temp->ptr_next != NULL)
+    {
+        if (temp->ptr_next->bloc_size == memory_size)
+        {
+            HEADER *temp2 = temp->ptr_next;
+            temp->ptr_next = temp2->ptr_next;
+            return temp2;
+        }
+        temp = temp->ptr_next;
+    }
+    return NULL;
 }
 
 int check_debo(HEADER *head)
@@ -78,7 +102,8 @@ int check_debo(HEADER *head)
 
 int main()
 {
-    // Validating first step
+    // Validating malloc_is3 function
+    printf("****************************************** Validating malloc_is3 function ******************************************\n");
     size_t size = 30;
     printf("magic_number : %ld\n", sizeof(MAGIC_NUMBER));
     printf("header : %ld\n", sizeof(HEADER));
@@ -99,12 +124,15 @@ int main()
     printf("init 3: %p\n", memory_alloc3);
     printf("end : %p\n", end);
 
-    // Validating second step
+    // Validating free_3is function
+    printf("****************************************** Validating free_3is function ******************************************\n");
     free_3is(memory_alloc0);
     free_3is(memory_alloc2);
     displayFreeMemoryBlock();
 
-    // Validating third step
+    // Validating check_debo function
+    printf("****************************************** Validating check_debo function ******************************************\n");
+    printf("//////////////// First usecase : ////////////////\n");
     int check_debo_test_1 = check_debo(memory_alloc3);
     if (check_debo_test_1)
     {
@@ -116,14 +144,23 @@ int main()
     }
 
     memory_alloc1->magic_number = 0x123456789;
+    printf("//////////////// Second usecase : ////////////////\n");
     int check_debo_test_2 = check_debo(memory_alloc1);
+
     if (check_debo_test_2)
-    { 
+    {
         printf("check debo is valid\n");
     }
     else
     {
         printf("check debo is not valid\n");
     }
+
+    // Validating reuseMemoryBlock function fitted with block size
+    printf("****************************************** Validating reuseMemoryBlock function ******************************************\n");
+    HEADER *memory_alloc4 = reuseMemoryBlock(size);
+    printf("Reuse memory with 30 as size block: %p | size=%ld\n", memory_alloc4, memory_alloc4->bloc_size);
+    displayFreeMemoryBlock();
+
     return 0;
 }
