b2e2abc37aa53703bb0d08f239415cd1ab5553bd
abderrazekbhr <abderrazek455@gmail.com>
2024-10-16 10:57:27 +0200

    all inodes with the hexa display

--- tp3/main.c
+++ tp3/main.c
@@ -28,9 +28,9 @@ int main()
 
     // ---------------- start display super blocks info ----------------
     printf("--------------------- Display super blocks info : --------------------- \n");
-    printf("Magic number = %d\n", superblock->magic);
-    printf("Block bitmap = %d\n", superblock->block_bitmap);
-    printf("Inode bitmap = %d\n", superblock->inode_bitmap);
+    printf("Magic number = " PRINTF_BINARY_PATTERN_INT32 "\n", PRINTF_BYTE_TO_BINARY_INT32(superblock->magic));
+    printf("Block bitmap = " PRINTF_BINARY_PATTERN_INT8 "\n", PRINTF_BYTE_TO_BINARY_INT8(superblock->block_bitmap));
+    printf("Inode bitmap = " PRINTF_BINARY_PATTERN_INT8 "\n", PRINTF_BYTE_TO_BINARY_INT8(superblock->inode_bitmap));
     printf("Block size = %d\n", superblock->block_size);
     printf("Blocks = %d\n", superblock->blocks);
     printf("Inodes = %d\n", superblock->inodes);
@@ -38,18 +38,22 @@ int main()
     // ---------------- end display super blocks info ----------------
 
     inode_table = (struct tosfs_inode *)(fsdata + TOSFS_BLOCK_SIZE);
-    struct tosfs_inode *root_inode = &inode_table[TOSFS_ROOT_INODE];
-    // ---------------- start display inode info ----------------
-    printf("--------------------- Display inode info : --------------------- \n");
-    printf("Inode = %d\n", root_inode->inode);
-    printf("Block number = %d\n", root_inode->block_no);
-    printf("User ID = %d\n", root_inode->uid);
-    printf("Group ID = %d\n", root_inode->gid);
-    printf("Mode = %d\n", root_inode->mode);
-    printf("Permission = %d\n", root_inode->perm);
-    printf("Size = %d\n", root_inode->size);
-    printf("Link = %d\n", root_inode->nlink);
-    // ---------------- end display inode info ----------------
+
+    for (int i = 0; i < superblock->inodes + 1; i++)
+    {
+        struct tosfs_inode *inode = &inode_table[i];
+        // ---------------- start display inode info ----------------
+        printf("--------------------- Display inode %d info : --------------------- \n", i);
+        printf("Inode = %d\n", inode->inode);
+        printf("Block number = " PRINTF_BINARY_PATTERN_INT8 "\n", PRINTF_BYTE_TO_BINARY_INT8(inode->block_no));
+        printf("User ID = %d\n", inode->uid);
+        printf("Group ID = %d\n", inode->gid);
+        printf("Mode = %d\n", inode->mode);
+        printf("Permission = " PRINTF_BINARY_PATTERN_INT16 "\n", PRINTF_BYTE_TO_BINARY_INT16(inode->perm));
+        printf("Size = %d\n", inode->size);
+        printf("Link = %d\n", inode->nlink);
+        // ---------------- end display inode info ----------------
+    }
 
     munmap(fsdata, size);
     close(fd);
